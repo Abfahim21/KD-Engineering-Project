024cc7acaa5f99171b5ce01deb79b182e4d719a6
Abfahim21 <abrarfahim21@gmail.com>
2022-12-18 16:19:25 +0600

    first commit

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,5 +1,5 @@
 let hamburger = document.querySelector('.hamburger');
         hamburger.onclick = function() {
         navBar = document.querySelector('.menu');
-        navBar.classList.toggle(".active");
+        navBar.classList.toggle('.active');
         }

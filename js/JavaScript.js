7f66e61b8109589e1bacbbaf9a0f8f82808f6f6d
Abfahim21 <abrarfahim21@gmail.com>
2022-12-18 18:38:10 +0600

    updated somehow

--- js/JavaScript.js
+++
@@ -1,5 +0,0 @@
-let hamburger = document.querySelector('.hamburger');
-        hamburger.onclick = function() {
-        navBar = document.querySelector('.menu');
-        navBar.classList.toggle('.active');
-        }
